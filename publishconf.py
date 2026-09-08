ebd5c7b969be526a4cb31854c022655a756fb5ff
alexandrebrianto <alexandre.brianto29@gmail.com>
2026-09-08 14:53:20 +0200

    ...

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://github.com/briantoalexandre/portfolio_pelican"
+SITEURL = ""
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
