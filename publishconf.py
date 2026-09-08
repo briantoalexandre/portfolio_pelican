bec09b7ff4006d5cf3d67e48e598408e00393ac0
briantoalexandre <alexandre.brianto29@gmail.com>
2026-09-08 14:10:39 +0200

    https to http

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://portfolio_pelican"
+SITEURL = "http://portfolio_pelican"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
